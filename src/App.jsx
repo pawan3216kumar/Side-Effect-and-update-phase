0cc89fb044c75640b082da6e5aed4badd11c5e80
pawan3216kumar <dhananjay965114@gmail.com>
2024-05-23 19:10:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Posts from "./components/Posts";
+import Users from "./components/Users";
+import App1 from "./components/padi/pagina";
+
+
+export default function App() {
+  return (
+    <>
+       {/* <Users /> */}
+      {/* <Posts /> */}
+      <App1/>
+
+    </>
+  );
+}
